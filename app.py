fde0f004cad1795cff94cb7673633c619332a1ed
Ridho Aryo <50721572+ridhoaryo@users.noreply.github.com>
2020-04-30 12:10:34 +0700

    30Apr2020

--- app.py
+++ app.py
@@ -25,12 +25,21 @@ def cancer_predict():
             float(input['meanconcavity']),
             float(input['worstconcavity'])
         ]
-        pred = gbc.predict([feature])[0]
-        pred_proba = gbc.predict_proba([feature])
-        pred_and_proba = f"{round(np.max(pred_proba)*100,2)}% {'BENIGN' if pred == 1 else 'NOT BENIGN'}"
+
+        scaled_feature = scaler.transform([feature])
+        feature_pca = pca10.transform(scaled_feature)
+
+        predpca = gr10.predict(feature_pca)[0]
+        pred_probapca = gr10.predict_proba(feature_pca)
+        endresult = f"{round(np.max(pred_probapca)*100,2)}% {'BENIGN' if predpca == 1 else 'NOT BENIGN'}"
+
+
+        # pred = gbc.predict([feature])[0]
+        # pred_proba = gbc.predict_proba([feature])
+        # pred_and_proba = f"{round(np.max(pred_proba)*100,2)}% {'BENIGN' if pred == 1 else 'NOT BENIGN'}"
 
         return render_template('result.html',
-        data=input, prediction=pred_and_proba, worstconcave=input['worstconcave'],
+        data=input, prediction=endresult, worstconcave=input['worstconcave'],
         worstperim=input['worstperim'], meanconcave=input['meanconcave'],
         worstradius=input['worstradius'], meanperim=input['meanperim'],
         worstarea=input['worstarea'], meanradius=input['meanradius'],
@@ -39,4 +48,8 @@ def cancer_predict():
 
 if __name__ == '__main__':
     gbc = joblib.load('gbc_breast_cancer')
+    gr10 = joblib.load("gr10") # load model ML GBC pca 2
+    scaler = joblib.load("scalerbreast10") # load si scaler
+    pca10 = joblib.load("pcabreast10") # load si pca
+
     app.run(debug=True, port=4000)
